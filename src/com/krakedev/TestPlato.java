8f939b0f7a0e8002193ce09c41ca7fe7250a1754
MelanyNono <melanynono123@gmail.com>
2026-08-18 16:33:33 -0500

    Crear clase Plato y TestPlato

---
+++ src/com/krakedev/TestPlato.java
@@ -0,0 +1,15 @@
+package com.krakedev;
+
+public class TestPlato {
+
+	public static void main(String[] args) {
+		
+		Plato platoA = new Plato();
+		
+		Plato platoB = new Plato();
+		
+		Plato platoC = new Plato();
+
+	}
+
+}
