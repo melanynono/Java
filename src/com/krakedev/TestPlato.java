cb1a9b5a1d800b832d441ae1125f9e87479cc147
MelanyNono <melanynono123@gmail.com>
2026-08-18 16:58:36 -0500

    Asignar valores a los atributos de los platos

--- src/com/krakedev/TestPlato.java
+++ src/com/krakedev/TestPlato.java
@@ -12,6 +12,11 @@ public class TestPlato {
 		System.out.println("Precio: "+ platoA.precio);
 		System.out.println("Disponible: "+ platoA.disponible);
 		
+		platoA.nombre = "Coctel de camarones";
+		platoA.tipo = "Entrada";
+		platoA.precio = 6.5;
+		platoA.disponible = true;
+		
 		Plato platoB = new Plato();
 		
 		System.out.println("=======Plato B========");
@@ -20,6 +25,11 @@ public class TestPlato {
 		System.out.println("Precio: "+ platoB.precio);
 		System.out.println("Disponible: "+ platoB.disponible);
 		
+		platoB.nombre = "Arroz marinero";
+		platoB.tipo = "Plato fuerte";
+		platoB.precio = 15.00;
+		platoB.disponible = true;
+		
 		Plato platoC = new Plato();
 		
 		System.out.println("=======Plato C========");
@@ -27,6 +37,11 @@ public class TestPlato {
 		System.out.println("Tipo: "+ platoC.tipo);
 		System.out.println("Precio: "+ platoC.precio);
 		System.out.println("Disponible: "+ platoC.disponible);
+		
+		platoC.nombre = "Tarta de queso";
+		platoC.tipo = "Postre";
+		platoC.precio = 6.00;
+		platoC.disponible = false;
 
 	}
 
