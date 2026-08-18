ff4599dafb5fd9d49642c7d9018e0c104ebb474e
MelanyNono <melanynono123@gmail.com>
2026-08-18 16:24:12 -0500

    Asignar valores de las variables

--- src/com/krakedev/TestRestaurante.java
+++ src/com/krakedev/TestRestaurante.java
@@ -12,6 +12,28 @@ public class TestRestaurante {
 		System.out.println("Direccion: " + rest1.direccion);
 		System.out.println("Calificacion: " + rest1.calificacion);
 		
+		rest1.nombre = "McDonald's";
+		rest1.direccion = "Centro Comercial Portal";
+		rest1.calificacion = 4.3;
+		
+		System.out.println("Nombre: " + rest1.nombre);
+		System.out.println("Direccion: " + rest1.direccion);
+		System.out.println("Calificacion: " + rest1.calificacion);
+		
+		Restaurante rest2 = new Restaurante();
+        System.out.println("--------Restaurante 2---------");
+		
+		System.out.println("Nombre: " + rest2.nombre);
+		System.out.println("Direccion: " + rest2.direccion);
+		System.out.println("Calificacion: " + rest2.calificacion);
+		
+		rest2.nombre = "Las menestras del Negro";
+		rest2.direccion = "Av 6 Diciembre";
+		rest2.calificacion = 4.4;
+		
+		System.out.println("Nombre: " + rest2.nombre);
+		System.out.println("Direccion: " + rest2.direccion);
+		System.out.println("Calificacion: " + rest2.calificacion);
 
 
 	}
