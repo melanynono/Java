b22812048cdbcd79840785aa50b695fbea0efd77
MelanyNono <melanynono123@gmail.com>
2026-08-18 16:47:37 -0500

    Instanciar los objetos

--- src/com/krakedev/TestPlato.java
+++ src/com/krakedev/TestPlato.java
@@ -6,9 +6,27 @@ public class TestPlato {
 		
 		Plato platoA = new Plato();
 		
+		System.out.println("=======Plato A========");
+		System.out.println("Nombre: "+ platoA.nombre);
+		System.out.println("Tipo: "+ platoA.tipo);
+		System.out.println("Precio: "+ platoA.precio);
+		System.out.println("Disponible: "+ platoA.disponible);
+		
 		Plato platoB = new Plato();
 		
+		System.out.println("=======Plato B========");
+		System.out.println("Nombre: "+ platoB.nombre);
+		System.out.println("Tipo: "+ platoB.tipo);
+		System.out.println("Precio: "+ platoB.precio);
+		System.out.println("Disponible: "+ platoB.disponible);
+		
 		Plato platoC = new Plato();
+		
+		System.out.println("=======Plato C========");
+		System.out.println("Nombre: "+ platoC.nombre);
+		System.out.println("Tipo: "+ platoC.tipo);
+		System.out.println("Precio: "+ platoC.precio);
+		System.out.println("Disponible: "+ platoC.disponible);
 
 	}
 
