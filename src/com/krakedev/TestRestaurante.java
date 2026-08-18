256b7e76e43b9e6f9f88e5708d6db3c6055b4f08
MelanyNono <melanynono123@gmail.com>
2026-08-18 16:21:18 -0500

    Crear class Restaurante

---
+++ src/com/krakedev/TestRestaurante.java
@@ -0,0 +1,19 @@
+package com.krakedev;
+
+public class TestRestaurante {
+
+	public static void main(String[] args) {
+		
+        Restaurante rest1 = new Restaurante();
+		
+		System.out.println("--------Restaurante 1---------");
+		
+		System.out.println("Nombre: " + rest1.nombre);
+		System.out.println("Direccion: " + rest1.direccion);
+		System.out.println("Calificacion: " + rest1.calificacion);
+		
+
+
+	}
+
+}
